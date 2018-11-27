3e9d2171e4c09f5967f7a2ba6b5eba9c3ca0331b
sophieclaisse <43751485+sophieclaisse@users.noreply.github.com>
2018-11-27 13:24:56 +0100

    memory v1

--- Memory.js
+++ Memory.js
@@ -61,20 +61,42 @@ shuffle(Tab);
 console.log(Tab2);
 
 
-// TO START THE GAME
+// TO START THE GAME - create img "recto"
 document.getElementById("play").addEventListener("click", function () {
 
     for (var j=0; j<Tab2.length; j++) {
-        document.getElementById("jeu"+[j]).innerHTML += '<img id="image" src="' + Tab2[j].verso + '" />'//A METTRE RECTO AU DEMARRAGE
+       var container= document.getElementById("jeu"+[j]);
+
+        var img = document.createElement("img");
+        img.className= "image";
+        img.src= Tab2[j].recto;
+
+        img.title=Tab2[j].verso;
+
+        container.appendChild(img);
+
+
     }
+    document.getElementById("play").disabled= true;
 });
 
 
+//EVENT- see img "verso"
+var cartes= document.getElementsByClassName("carte");
 
-document.getElementById("image").addEventListener("click", function(){
+for (var x=0; x<cartes.length; x++) {
+    cartes[x].addEventListener("click", function (e) {
+       console.log(e.target);
+
+       e.target.src= e.target.title;
+
+       //e.target.innerHTML = '<img class="image" src="' + Tab2[x].recto + '" />';
+
+    });
+
+}
 
 
-});
 
 
 
