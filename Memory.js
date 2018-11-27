73f51ac0b6dfc02a53b8fd93175e8b659d6bda08
sophieclaisse <43751485+sophieclaisse@users.noreply.github.com>
2018-11-27 15:53:05 +0100

    memory v1

--- Memory.js
+++ Memory.js
@@ -21,17 +21,9 @@ Tab.push(new jeu("VISUELS/Face.jpg","VISUELS/Pingouin.png","Pingouin"));
 Tab.push(new jeu("VISUELS/Face.jpg","VISUELS/Pingouin.png","Pingouin"));
 //console.log(Tab);
 
-//UPLOAD FUNCTION
-/*function base () {
-    for (var i = 0; i <= Tab.length; i++) {
-        document.getElementById("jeu").innerHTML += '<img id="image" src="' + Tab[i].recto + '" />';
-
-    }
-}
-base();
-*/
 
 var Tab2= [];
+var TabEvent= [];
 
 var rand;
 
@@ -58,48 +50,108 @@ function shuffle(param){
 
 shuffle(Tab);
 
-console.log(Tab2);
+//console.log(Tab2);
 
 
-// TO START THE GAME - create img "recto"
-document.getElementById("play").addEventListener("click", function () {
+// FUNCTION TO START THE GAME - create img "recto"
+function start () {
 
-    for (var j=0; j<Tab2.length; j++) {
-       var container= document.getElementById("jeu"+[j]);
+    document.getElementById("play").addEventListener("click", function () {
 
-        var img = document.createElement("img");
-        img.className= "image";
-        img.src= Tab2[j].recto;
+        for (var j = 0; j < Tab2.length; j++) {
+            var container = document.getElementById("jeu" + [j]);
 
-        img.title=Tab2[j].verso;
+            var img = document.createElement("img");
+            img.className = "image";
+            img.src = Tab2[j].recto;
 
-        container.appendChild(img);
+            img.name = Tab2[j].verso;
+            img.title = Tab2[j].titre;
 
-
-    }
-    document.getElementById("play").disabled= true;
-});
+            container.appendChild(img);
 
 
-//EVENT- see img "verso"
-var cartes= document.getElementsByClassName("carte");
-
-for (var x=0; x<cartes.length; x++) {
-    cartes[x].addEventListener("click", function (e) {
-       console.log(e.target);
-
-       e.target.src= e.target.title;
-
-       //e.target.innerHTML = '<img class="image" src="' + Tab2[x].recto + '" />';
-
+        }
+        document.getElementById("play").disabled = true;
     });
+}
+start();
+
+//FUNCTION PLAY - EVENT- see img "verso"
+var result= 0;
+
+var test=2;
+function playGame() {
+
+        var cartes = document.getElementsByClassName("carte");
+
+        for (var x = 0; x < cartes.length; x++) {
+            cartes[x].addEventListener("click", function (e) {
+                //console.log(e.target);
+
+                e.target.src = e.target.name;
+
+                result= e.target.title;//reaction
+                TabEvent.push(result);
+                //console.log(TabEvent);
+                test--;
+                console.log(test);
+
+                if (test==0 && TabEvent[0]== TabEvent[1] ){
+                    alert ("gagné");
+
+                }
+                if (test==0 && TabEvent[0]!= TabEvent[1] ) {
+                    alert ("perdu");
+
+                }
+            });
+        }
 
 }
+playGame();
 
 
 
 
 
+
+
+/*while (TabEvent.length>0 && TabEvent.length<2){
+
+                   if (Tab[0]== TabEvent[1] && Tab[0]!=0){
+                       alert ("gagné");
+                   }
+
+                   if (TabEvent[0]!= TabEvent[1] && TabEvent[1]!=0){
+                       alert ("perdu");
+                       e.target.src= img.src;
+
+                   }
+               }*/
+
+/*for (var y=0; y<=2); y++){
+
+    if (TabEvent[0].result==TabEvent[1]){
+        alert ("gagné");
+    }
+    else {
+        alert ("perdu");
+    }
+}*/
+
+
+/*function reset (){
+
+    document.getElementById("reset").addEventListener("click", function (){
+
+        shuffle(Tab);
+        playGame();
+
+
+    });
+}
+*/
 
 
 
